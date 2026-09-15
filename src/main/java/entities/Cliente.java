7c0e0ae11d28ec54006841ab29a89007f0897c8b
Gabriel Pinheiro <SEU_EMAIL_DO_GITHUB>
2026-09-15 11:10:28 -0300

    Entidade Cliente

--- src/main/java/entities/Cliente.java
+++ src/main/java/entities/Cliente.java
@@ -1,4 +1,33 @@
 package entities;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Entity
+@Table(name = "clientes")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@ToString
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O campo nome é obrigatório!")
+    @Column(nullable = false)
+    private String nome;
+
+    private String telefone;
+
+    @NotBlank(message = "O campo e-mail é obrigatório!")
+    @Email(message = "O e-mail é invalido!")
+    @Column(nullable = false)
+    private String email;
+    
 }
